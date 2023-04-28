2d814a9e0efdf936ad54fef6760e15d293733d89
JudexMars <danielpro2013rus@gmail.com>
2023-04-28 23:18:52 +0300

    Init

---
+++ src/test/java/judexdev/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package judexdev.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
