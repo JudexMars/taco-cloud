4012328c5b0e261ef5d864f281e5325841ee43ae
JudexMars <danielpro2013rus@gmail.com>
2023-05-02 01:30:53 +0300

    Replaced explicit HomeController class with web configuration

--- src/test/java/judexdev/tacocloud/HomeControllerTest.java
+++ src/test/java/judexdev/tacocloud/HomeControllerTest.java
@@ -10,14 +10,13 @@ import static
 import static
         org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import judexdev.tacocloud.controllers.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 
-@WebMvcTest (HomeController.class)
+@WebMvcTest
 public class HomeControllerTest {
 
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
